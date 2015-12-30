64dea3cfb6030dc20a6db19ee984fc10ad6ed8b8
Lucas Andrey <andreybleme1@gmail.com>
2015-12-30 09:53:01 -0200

    Initial angular structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apodApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the apodApp
+ */
+angular.module('apodApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
